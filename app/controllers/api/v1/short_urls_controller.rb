8e39ea85c212b51d09032ceb420d9953a655c94d
dicarlomtz <dicarlomtz@gmail.com>
2022-11-12 19:52:32 -0600

    Adding index method to retrieved top 100 urls by clicks count

--- app/controllers/api/v1/short_urls_controller.rb
+++ app/controllers/api/v1/short_urls_controller.rb
@@ -1,9 +1,14 @@
+require 'json'
+
 class ShortUrlsController < ApplicationController
 
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
   def index
+    @short_urls = ShortUrl.order(:click_count)
+                          .take(100)
+    render json: JSON.generate({ urls: @short_urls.to_json })
   end
 
   def create
