ce0b27a1244a738db334cd90b2d4c755a9a58a03
dicarlomtz <dicarlomtz@gmail.com>
2022-11-12 19:49:30 -0600

    Removing unnecessary validation

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -3,7 +3,6 @@ class ShortUrl < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }, allow_nil: true
   validates :full_url, url: true, presence: true, uniqueness: { case_sensitive: false }
   validates :short_code, presence: true, uniqueness: { case_sensitive: true }
-  validates :full_url, url: true, if: :full_url_changed?
   before_validation :add_short_code
 
   def increment_clicks!
