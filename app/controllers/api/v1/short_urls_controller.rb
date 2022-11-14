ab1c8176d6ec0048aafcb8276cd333819f5074ae
dicarlomtz <dicarlomtz@gmail.com>
2022-11-13 20:50:40 -0600

    Updating endpoint responses

--- app/controllers/api/v1/short_urls_controller.rb
+++ app/controllers/api/v1/short_urls_controller.rb
@@ -1,5 +1,3 @@
-require 'json'
-
 class Api::V1::ShortUrlsController < ApplicationController
 
   # Since we're working on an API, we don't have authenticity tokens
@@ -9,7 +7,7 @@ class Api::V1::ShortUrlsController < ApplicationController
   def index
     @short_urls = ShortUrl.order(click_count: :desc)
                           .take(100)
-    render json: JSON.generate({ urls: @short_urls.to_json })
+    render json: { urls: @short_urls.to_json }
   end
 
   # Creates a new short url
@@ -18,13 +16,13 @@ class Api::V1::ShortUrlsController < ApplicationController
     @short_url = ShortUrl.create(full_url: params[:full_url])
     if @short_url.valid?
       UpdateTitleJob.perform_later(@short_url.id)
-      render json: JSON.generate({short_url: "#{request.host_with_port}/#{@short_url.short_code}"}), status: :created
+      render json: {short_code: @short_url.short_code}, status: :created
     else
       @previously_added = ShortUrl.find_by_full_url(params[:full_url])
       unless @previously_added.nil?
-        render json: JSON.generate({short_url: "#{request.host_with_port}/#{@previously_added.short_code}"}), status: :created
+        render json: {short_code: @previously_added.short_code}, status: :created
       else
-        render json: JSON.generate({ errors: @short_url.errors.to_json }), status: :unprocessable_entity
+        render json: {errors: @short_url.errors}, status: :unprocessable_entity
       end
     end
   end
@@ -37,7 +35,7 @@ class Api::V1::ShortUrlsController < ApplicationController
       redirect_to @short_url.full_url
       @short_url.increment_clicks!
     else
-      redirect_to '/404.html'
+      render json: {}, status: :not_found
     end
   end
 
